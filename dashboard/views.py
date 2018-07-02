57960ba7b93d202ee30dc25aa5c92c00f0f503e2
surya-shodan <suryakumar.sudar@gmail.com>
2018-07-02 16:00:29 +0530

    Init Commit

---
+++ dashboard/views.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from pymongo import version as py_version
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class MainAPI(APIView):
+    def get(self, request, format=None):
+        return Response({'pymongo_version': py_version}, status=200)
